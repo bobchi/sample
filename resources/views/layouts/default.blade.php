c2f574e99ec4a87194da2280598bb7baa7c29d1d
bobchi <bobchi@163.com>
2018-04-17 23:48:55 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,9 +2,14 @@
 <html>
  <head>
   <title>@yield('title','Sample') - hello Laravel</title>
+  <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
+@include('layouts._header')
+<div class="container">
  @yield('content')
+ @include('layouts._footer')
+</div>
 </body>
 </html>
 
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,5 +2,16 @@
 @section('title','主页')
 
 @section('content')
- <h1>主页</h1>
+ <div class="jumbotron">
+  <h1>Hello Laravel</h1>
+  <p>
+   这是Sample App项目首页
+  </p>
+  <p>
+   一切从这里开始
+  </p>
+  <p>
+   <a href="{{ route('signup') }}" class="btn btn-lg btn-success" role="button">现在注册</a>
+  </p>
+ </div>
 @stop

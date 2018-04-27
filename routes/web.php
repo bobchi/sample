b096c7a93b3a27626914a0b8c6d4f736baa5bbc7
bobchi <bobchi@163.com>
2018-04-27 00:00:07 +0000

    Finish login and logout

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
-Route::get('/signup', 'UsersController@create')->name('signup');
+Route::get('signup', 'UsersController@create')->name('signup');
 
 Route::resource('users','UsersController');
+
+Route::get('login', 'SessionController@create')->name('login');
+Route::post('login', 'SessionController@store')->name('login');
+Route::delete('login', 'SessionController@destroy')->name('logout');
